b4c464b6d9269dec6743ce442522e1085e94cfd2
Jessica Patterson <jessica@theredsunflower.com>
2023-02-18 17:44:57 -0500

    Update index.php
    simplified page

--- index.php
+++ index.php
@@ -11,30 +11,31 @@ get_header(); ?>
 
 <div id="page">
 	<div id="content">
-		<div id="section-one">
-	    	<h1>Section One</h1>
-	    	<img id="" src="http://codecamp.theredsunflower.com/wp-content/uploads/2018/07/dank-memes-face.png">
-	    	<a id="" href="">Learn More</a>
-	    	<p id="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue mauris libero, mattis dapibus tortor commodo quis. Nam libero urna, fringilla id dictum in, sagittis sit amet justo. Duis iaculis elit ac metus volutpat, sed consequat eros volutpat. Ut ac metus lobortis, cursus diam nec, dignissim erat. Pellentesque vitae tincidunt eros. Vestibulum et luctus est, et pharetra neque. Vivamus viverra tristique velit a imperdiet. Curabitur vitae odio vitae lectus faucibus aliquet ut vitae odio. Nam egestas porttitor neque, posuere gravida nisl ultricies et. Phasellus lobortis tellus sed tortor fermentum, finibus efficitur lacus pharetra. Suspendisse volutpat pharetra lorem eu pulvinar. Vestibulum auctor nulla est, quis accumsan odio iaculis ut. Phasellus sagittis mauris sit amet fermentum suscipit. Nullam vehicula nulla orci, sed mattis tellus ornare ac.</p>
-		</div>
-		<div id="section-two">
-	    	<h1>Section Two</h1>
-	    	<img id="image-two" src="http://codecamp.theredsunflower.com/wp-content/uploads/2018/07/giphyy.gif">
-	    	<a id="" href="">Learn More</a>
-	    	<p id="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue mauris libero, mattis dapibus tortor commodo quis. Nam libero urna, fringilla id dictum in, sagittis sit amet justo. Duis iaculis elit ac metus volutpat, sed consequat eros volutpat. Ut ac metus lobortis, cursus diam nec, dignissim erat. Pellentesque vitae tincidunt eros. Vestibulum et luctus est, et pharetra neque. Vivamus viverra tristique velit a imperdiet. Curabitur vitae odio vitae lectus faucibus aliquet ut vitae odio. Nam egestas porttitor neque, posuere gravida nisl ultricies et. Phasellus lobortis tellus sed tortor fermentum, finibus efficitur lacus pharetra. Suspendisse volutpat pharetra lorem eu pulvinar. Vestibulum auctor nulla est, quis accumsan odio iaculis ut. Phasellus sagittis mauris sit amet fermentum suscipit. Nullam vehicula nulla orci, sed mattis tellus ornare ac.</p>
-		</div>
-		<div id="section-three">
-	    	<h1>Section Three</h1>
-	    	<img id="" src="http://codecamp.theredsunflower.com/wp-content/uploads/2018/07/giphy-2.gif">
-	    	<a id="" href="">Learn More</a>
-	    	<p id="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue mauris libero, mattis dapibus tortor commodo quis. Nam libero urna, fringilla id dictum in, sagittis sit amet justo. Duis iaculis elit ac metus volutpat, sed consequat eros volutpat. Ut ac metus lobortis, cursus diam nec, dignissim erat. Pellentesque vitae tincidunt eros. Vestibulum et luctus est, et pharetra neque. Vivamus viverra tristique velit a imperdiet. Curabitur vitae odio vitae lectus faucibus aliquet ut vitae odio. Nam egestas porttitor neque, posuere gravida nisl ultricies et. Phasellus lobortis tellus sed tortor fermentum, finibus efficitur lacus pharetra. Suspendisse volutpat pharetra lorem eu pulvinar. Vestibulum auctor nulla est, quis accumsan odio iaculis ut. Phasellus sagittis mauris sit amet fermentum suscipit. Nullam vehicula nulla orci, sed mattis tellus ornare ac.</p>
-		</div>
-		<div id="section-four">
-	    	<h1>Section Four</h1>
-	    	<img id="" src="http://codecamp.theredsunflower.com/wp-content/uploads/2018/07/stub-toe.png">
-	    	<a id="" href="">Learn More</a>
-	    	<p id="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue mauris libero, mattis dapibus tortor commodo quis. Nam libero urna, fringilla id dictum in, sagittis sit amet justo. Duis iaculis elit ac metus volutpat, sed consequat eros volutpat. Ut ac metus lobortis, cursus diam nec, dignissim erat. Pellentesque vitae tincidunt eros. Vestibulum et luctus est, et pharetra neque. Vivamus viverra tristique velit a imperdiet. Curabitur vitae odio vitae lectus faucibus aliquet ut vitae odio. Nam egestas porttitor neque, posuere gravida nisl ultricies et. Phasellus lobortis tellus sed tortor fermentum, finibus efficitur lacus pharetra. Suspendisse volutpat pharetra lorem eu pulvinar. Vestibulum auctor nulla est, quis accumsan odio iaculis ut. Phasellus sagittis mauris sit amet fermentum suscipit. Nullam vehicula nulla orci, sed mattis tellus ornare ac.</p>
-		</div>
+		<?php
+
+		// checks if there are any posts that match the query
+		if (have_posts()) :
+
+		  // If there are posts matching the query then start the loop
+		  while ( have_posts() ) : the_post();
+
+		    // the code between the while loop will be repeated for each post
+		    ?>
+			<h1 class="page-title" id="post-<?php the_ID(); ?>"><?php the_title(); ?></h1>
+		    <?php the_content(); ?>
+
+		    <?php
+
+		    // Stop the loop when all posts are displayed
+		 endwhile;
+
+		// If no posts were found
+		else :
+		?>
+		<p>Sorry no posts matched your criteria.</p>
+		<?php
+		endif;
+		?>
 	</div><!--content-->
 </div><!--page-->
 
