d2a312c6e2c84af34029c733bdc62db8400b0656
Jessica Patterson <jessica@theredsunflower.com>
2023-02-18 17:43:02 -0500

    Update functions.php
    removed extra menus and changed package name

--- functions.php
+++ functions.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * tulip functions and definitions
+ * codecamp functions and definitions
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package tulip
+ * @package codecamp
  */
 
-if ( ! function_exists( 'tulip_setup' ) ) :
+if ( ! function_exists( 'codecamp_setup' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
@@ -15,14 +15,14 @@ if ( ! function_exists( 'tulip_setup' ) ) :
 	 * runs before the init hook. The init hook is too late for some features, such
 	 * as indicating support for post thumbnails.
 	 */
-	function tulip_setup() {
+	function codecamp_setup() {
 		/*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on tulip, use a find and replace
-		 * to change 'tulip' to the name of your theme in all the template files.
+		 * If you're building a theme based on codecamp, use a find and replace
+		 * to change 'codecamp' to the name of your theme in all the template files.
 		 */
-		load_theme_textdomain( 'tulip', get_template_directory() . '/languages' );
+		load_theme_textdomain( 'codecamp', get_template_directory() . '/languages' );
 
 		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
@@ -44,9 +44,7 @@ if ( ! function_exists( 'tulip_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'tulip' ),
-			'menu-2' => esc_html__( 'library', 'tulip' ),
-			'menu-3' => esc_html__( 'blog', 'tulip' ),
+			'menu-1' => esc_html__( 'Primary', 'codecamp' )
 		) );
 
 		/*
@@ -61,7 +59,7 @@ if ( ! function_exists( 'tulip_setup' ) ) :
 			'caption',
 		) );
 		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'tulip_custom_background_args', array(
+		add_theme_support( 'custom-background', apply_filters( 'codecamp_custom_background_args', array(
 			'default-color' => 'ffffff',
 			'default-image' => '',
 		) ) );
@@ -82,7 +80,7 @@ if ( ! function_exists( 'tulip_setup' ) ) :
 		) );
 	}
 endif;
-add_action( 'after_setup_theme', 'tulip_setup' );
+add_action( 'after_setup_theme', 'codecamp_setup' );
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -91,44 +89,44 @@ add_action( 'after_setup_theme', 'tulip_setup' );
  *
  * @global int $content_width
  */
-function tulip_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'tulip_content_width', 640 );
+function codecamp_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'codecamp_content_width', 640 );
 }
-add_action( 'after_setup_theme', 'tulip_content_width', 0 );
+add_action( 'after_setup_theme', 'codecamp_content_width', 0 );
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function tulip_widgets_init() {
+function codecamp_widgets_init() {
 	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'tulip' ),
+		'name'          => esc_html__( 'Sidebar', 'codecamp' ),
 		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'tulip' ),
+		'description'   => esc_html__( 'Add widgets here.', 'codecamp' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
 }
-add_action( 'widgets_init', 'tulip_widgets_init' );
+add_action( 'widgets_init', 'codecamp_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
  */
-function tulip_scripts() {
-	wp_enqueue_style( 'tulip-style', get_stylesheet_uri() );
+function codecamp_scripts() {
+	wp_enqueue_style( 'codecamp-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'tulip-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'codecamp-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'tulip-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'codecamp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-add_action( 'wp_enqueue_scripts', 'tulip_scripts' );
+add_action( 'wp_enqueue_scripts', 'codecamp_scripts' );
 
 /**
  * Implement the Custom Header feature.
