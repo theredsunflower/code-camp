b7c49692c182affb18c17b43ef1d5ef270ba02c5
Jessica Patterson <jessica@theredsunflower.com>
2023-02-18 17:44:04 -0500

    Update header.php
    Display logo, blog name, and description in header

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package tulip
+ * @package codecamp
  */
 
 ?>
@@ -17,6 +17,9 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet"> 
 
 	<?php wp_head(); ?>
 </head>
@@ -24,16 +27,35 @@
 <body <?php body_class(); ?>>
 
 	<header id="header">
-		<div id="sitename">
-			<h1>Code Camp</h1>
+		<div id="logo">
+			<?php
+				$custom_logo_id = get_theme_mod( 'custom_logo' );
+				$image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+				if(!($image[0] == '')) {
+					echo '<a href="/"><img src="'.$image[0].'" alt="logo"></a>';
+				}			
+			?>
 		</div>
+		<div id="sitename">
+			<?php 
+			//get blog name and description/tagline and display them
+			$blogname = get_bloginfo('name');
+			$blogdesc = get_bloginfo('description');
+				echo '<h1>'.$blogname.'</h1>';
+				echo '<p>'.$blogdesc.'</p>';
+			?>
+		</div>
+		<?php 
+		//only display the menu if home page is set to a static page
+		if(!is_home()) {?>
+		<nav>
+			<div id="menu">
+	            <?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
+	       	</div>
+		</nav><!-- #site-navigation -->
+		<?php } ?>
 
-	<!--<nav>
-		<div id="menu">
-			<h2>Menu</h2>-->
-            <?php /*wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); */?>
-        <!--</div>
-	</nav>--><!-- #site-navigation -->
+
 	
 	</header><!-- #masthead -->
 	
